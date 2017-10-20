33f6b98f8cdadddba92c16f7c1e138da5d06796f
estevao <estevaooliveira@my.smccd.edu>
2017-10-20 14:31:27 -0700

    App working

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '0uYDUStwVjnhVJIuMafQ2Fuie',
+  consumer_secret: 'eDIiEPebsMHaHr7YgZmfdy3QBL7JlUWtL8btRYz1jrCQdBBnpG',
+  access_token_key: '920420000153747457-ArmGp5ogswayE4io99DD8PMbJIfZLgn',
+  access_token_secret: 'E5bWT3WjZRJWsadkf8K2WtmxDdt3NPLEpCM6WiVMbIrdk',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,123 @@
+var inquirer = require('inquirer');
+var request = require('request');
+
+
+
+
+
+
+        if(process.argv[2]=='tweets'){
+            twitter();
+        }
+
+        else if(process.argv[2]=='spotify'){
+            if(process.argv[3]===undefined){ var song = 'dancing+queen';}
+            else{var song = process.argv.slice(3).join("+");}
+
+            spotify(song);
+        }
+
+
+
+        else if(process.argv[2]=='movie'){
+            if(process.argv[3]===undefined)
+                {var title="mr.nobody";}
+            else{var title = process.argv.slice(3).join("+");}
+            
+            movie(title);
+
+        }
+        else if(process.argv[2]=='do-this'){
+            console.log("the random.txt file can save and run the same commands that you would enter as arguments into the terminal.");
+            console.log(" For example, try entering: 'movie the lion king', or 'tweets', or 'spotify man in the mirror'.");
+            console.log("");
+            var fs = require('fs');
+            fs.readFile('random.txt', 'utf8', function(err, data){
+                if(err){
+                    console.log(err);
+                    return console.error(err);
+                }
+                var dataArr = data.split(" ");
+
+                if(dataArr[0]=='tweets')
+                    {twitter();}
+                else if(dataArr[0]=='spotify')
+                    {var song = dataArr.slice(1).join("+");
+                        spotify(song);}
+                else if(dataArr[0]=='movie')
+                    {var title = dataArr.slice(1).join("+");
+                        movie(title);}
+            });
+
+
+
+        }else {console.log("Invalid command. Correct commands are:");
+                console.log("tweets");
+                console.log("'spotify' then the song name");
+                console.log("'movie' then the movie title");
+                console.log("'do-this' to use the text file command.");}
+  // do something with data 
+
+  function twitter(){
+    console.log('Loading...');
+            var Twitter = require('twitter');
+            var client = new Twitter({
+              consumer_key: '0uYDUStwVjnhVJIuMafQ2Fuie',
+              consumer_secret: 'eDIiEPebsMHaHr7YgZmfdy3QBL7JlUWtL8btRYz1jrCQdBBnpG',
+              access_token_key: '920420000153747457-ArmGp5ogswayE4io99DD8PMbJIfZLgn',
+              access_token_secret: 'E5bWT3WjZRJWsadkf8K2WtmxDdt3NPLEpCM6WiVMbIrdk',
+            });
+
+            var params = {screen_name: 'SoyStevee'};
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+              if (!error) {
+                for(var i=0; i<20;i++)
+                    {console.log(tweets[i].text);}
+              } else{console.log(error);}
+            });
+  }
+
+  function spotify(song){
+    var Spotify = require('node-spotify-api');
+            var spotify = new Spotify({
+              id: '679ee935046141a2b61359db189c8f0f',
+              secret: '43be1ff3290341ca93c477df462b167a'
+            });
+             
+            spotify.search({ type: 'track', query: song }, function(err, data) {
+              if (err) {
+                return console.log('Error occurred: ' + err);
+              }
+                var songInfo = data.tracks.items[0]; 
+                console.log("Artist: "+songInfo.artists[0].name);
+                console.log("Song: "+songInfo.name);
+                console.log("Album: "+songInfo.album.name);
+                console.log("Preview Link: "+songInfo.preview_url); 
+            });
+  }
+
+  function movie(title){
+
+            
+            // if (movie== undefined){movie = "meet+joe+black";}
+            var queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece";
+
+            var request = require('request');
+
+            request(queryURL, function(error, response, body){
+            if(!error && response.statusCode === 200){
+                var obj = JSON.parse(body);
+                console.log("Title of Movie "+obj.Title);
+                console.log("Year of Movie: "+obj.Year);
+                console.log("IMBD Rating: "+obj.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: "+obj.Ratings[1].Value);
+                console.log("Country Movie was Produced: "+obj.Country);
+                console.log("Language of Movie: "+obj.Language);
+                console.log("Plot of Move: "+obj.Plot);
+                console.log("Actors in Movie: "+ obj.Actors);
+
+                //console.log(obj);
+            }else {console.log(error);}
+
+        });
+  }
